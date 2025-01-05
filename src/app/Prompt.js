7aa32a08d8d2c22e9c49489430179cc69ba15daa
Mariano Rodriguez <amr001.us@proton.me>
2025-01-05 14:28:06 -0600

    Fix bug affecting WebKit browsers where map is not defined on an iterable

--- src/app/Prompt.js
+++ src/app/Prompt.js
@@ -1,23 +1,39 @@
-'use client'
+"use client";
 
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Prompt({ cmd }) {
-  let path = usePathname();
+	let path = usePathname();
 
-  if (path == "/") {
-    path = ""
-  }
+	if (path == "/") {
+		path = "";
+	}
 
-  let pathSections = path.split("/");
-  let breadcrumbs = pathSections.entries().map(([i, item]) => ( <><Link className="hover:underline" key={i} href={pathSections.slice(0, i+1).join("/")} >{item}</Link>/</> ));
+	let pathSections = path.split("/");
+	let breadcrumbs = Array.from(pathSections.entries()).map(([i, item]) => (
+		<>
+			<Link
+				className="hover:underline"
+				key={i}
+				href={pathSections.slice(0, i + 1).join("/")}
+			>
+				{item}
+			</Link>
+			/
+		</>
+	));
 
-  return (
-    <div className={`font-mono`}>
-      <span className="max-sm:hidden ">mariano@portfolio: </span>
-      <span className="text-secondary"><Link className="hover:underline" href="/">home</Link>{ ...breadcrumbs }</span>
-      <span>$ { cmd }</span>
-    </div>
-  );
+	return (
+		<div className={`font-mono`}>
+			<span className="max-sm:hidden ">mariano@portfolio: </span>
+			<span className="text-secondary">
+				<Link className="hover:underline" href="/">
+					home
+				</Link>
+				{...breadcrumbs}
+			</span>
+			<span>$ {cmd}</span>
+		</div>
+	);
 }
