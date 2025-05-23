8588d8042d8a550a70ca2834542b9688dd91763c
Mariano Rodriguez <amr001.us@proton.me>
2025-05-22 22:59:51 -0500

    fix a regression with loader

--- src/app/Loading.js
+++ src/app/Loading.js
@@ -1,35 +1,33 @@
 import logs from "./logs";
-import {useState, useEffect, useRef} from "react";
-import styles from "./Loading.module.css"
+import { useState, useEffect, useRef } from "react";
+import styles from "./Loading.module.css";
 
 export default function Loading({ onComplete }) {
-  const logEndRef = useRef(null)
-  const [visibleLogs, setVisibleLogs] = useState("");
-  
-  useEffect(() => {
-    let logIndex = 0;
-    const interval = setInterval(() => {
-      if (logIndex < logs.length - 1) {
-        setVisibleLogs((prevLogs) => `${prevLogs}${logs[logIndex]}\n`);
-        logIndex++;
-        if (logEndRef.currrent != null) {
-          logEndRef.current.scrollIntoView()
-        }
-      } else {
-        onComplete();
-        clearInterval(interval);
-      }
-    }, 15);
+	const logEndRef = useRef(null);
+	const [visibleLogs, setVisibleLogs] = useState("");
 
-    return () => clearInterval(interval);
-  }, [onComplete]);
+	useEffect(() => {
+		let logIndex = 0;
+		const interval = setInterval(() => {
+			if (logIndex < logs.length - 1) {
+				setVisibleLogs((prevLogs) => `${prevLogs}${logs[logIndex]}\n`);
+				logIndex++;
+				logEndRef.current.scrollIntoView();
+			} else {
+				onComplete();
+				clearInterval(interval);
+			}
+		}, 15);
 
-  return (
-    <>
-      <div className="font-mono">
-        <pre>{visibleLogs}</pre>
-      </div>
-      <div ref={logEndRef}></div>
-    </>
-  );
+		return () => clearInterval(interval);
+	}, [onComplete]);
+
+	return (
+		<>
+			<div className="font-mono">
+				<pre>{visibleLogs}</pre>
+			</div>
+			<div ref={logEndRef}></div>
+		</>
+	);
 }
